c05659b0d3ca6eafe14296d233c1e7e8b313a438
kstoyanov135 <121305077+kstoyanov135@users.noreply.github.com>
2022-12-29 14:46:16 +0200

    Add files via upload

---
+++ Show.Hide elements/index.js
@@ -0,0 +1,11 @@
+const myButton = document.getElementById('myButton');
+const myImg = document.getElementById('myImg');
+
+myButton.addEventListener('click', () =>{
+    if(myImg.style.display == "none") {
+        myImg.style.display = "block";
+    }
+    else {
+        myImg.style.display = "none";
+    }
+})

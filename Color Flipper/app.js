9f4280399a5355d0dfba2f31db6b38f2efd4a1ce
kstoyanov135 <121305077+kstoyanov135@users.noreply.github.com>
2023-01-03 15:59:01 +0200

    Update app.js

--- Color Flipper/app.js
+++ Color Flipper/app.js
@@ -1,14 +1,14 @@
-const colors = ["green", "red", "rgba(133, 122, 200", "#f15025"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+const colors = ["green", "red", "rgba(133, 122, 200", "#f15025"]; // an array of colors
+const btn = document.getElementById("btn"); // HTML element with an ID of 'btn'
+const color = document.querySelector(".color"); // element with a class of 'color'
 
-btn.addEventListener('click', function(){
-    // ger random number between 0 - 3 
+btn.addEventListener('click', function(){ // when we click the button
+    // get a random number between 0 - 3
     const randomNumber = getRnadomNumber();
     document.body.style.backgroundColor = colors[randomNumber]; // DOM
     color.textContent = colors[randomNumber];
 });
 
 function getRnadomNumber(){
-    return Math.floor(Math.random() * colors.length);
+    return Math.floor(Math.random() * colors.length); // we use the array to get a raondom color
 }

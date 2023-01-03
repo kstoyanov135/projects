d37ed469248f3e82c190a8aab3a4413fcfb4ee3c
kstoyanov135 <121305077+kstoyanov135@users.noreply.github.com>
2023-01-03 15:16:47 +0200

    Update index.js

--- Clock/index.js
+++ Clock/index.js
@@ -1,28 +1,28 @@
-const myLabel = document.getElementById('myLabel');
+const myLabel = document.getElementById('myLabel'); //assigns the value of the element in the HTML document with the ID 'myLabel'
 
-update();
-setInterval(update, 1000);
+update(); // calls the update function
+setInterval(update, 1000); // a method that repeatedly calls a function with a fixed time delay
 
 function update() {
-    let date = new Date();
-    myLabel.innerHTML = formatTime(date);
+    let date = new Date(); // creates a new 'Date' object, that represents the current time and date
+    myLabel.innerHTML = formatTime(date); // the formatTime function returns a string that represents the current time in the format 'hh:mm:ss AM/PM"
 
     function formatTime(date){
-        let hours = date.getHours();
-        let minutes = date.getMinutes();
-        let seconds = date.getSeconds();
-        let amOrPm = hours >=12 ? "PM" : "AM";
+        let hours = date.getHours(); //gets the hours
+        let minutes = date.getMinutes(); //gets the minutes
+        let seconds = date.getSeconds(); //gets the seconds
+        let amOrPm = hours >=12 ? "PM" : "AM"; //determines whether it is AM or PM
 
-        hours = (hours % 12) || 12;
+        hours = (hours % 12) || 12; // converts the hours to a 12 hour format
 
-        hours = formatZeroes(hours);
-        minutes = formatZeroes(minutes);
-        seconds = formatZeroes(seconds);
+        hours = formatZeroes(hours); //adds a leading zero to the hours
+        minutes = formatZeroes(minutes); //adds a leading zero to the minutes
+        seconds = formatZeroes(seconds); //adds a leading zero to the seconds
 
-        return `${hours}:${minutes}:${seconds} ${amOrPm}`;
+        return `${hours}:${minutes}:${seconds} ${amOrPm}`; // returns the formatted time as a string
     }
     function formatZeroes(time){
-        time = time.toString();
-        return time.length < 2 ? "0" + time : time;
+        time = time.toString(); // converts the value of 'time' to a string because the length property can only be used on strings
+        return time.length < 2 ? "0" + time : time; // ternary operator to return 0 + time or just 'time'
     }
 }
